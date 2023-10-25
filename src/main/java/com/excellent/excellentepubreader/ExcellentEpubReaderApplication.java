fb59e549e0666a47a15ccc9f1da35fdbca8a555a
ChisKien <chiskien03214@gmail.com>
2023-10-25 20:24:28 +0700

    Excellent Epub Reader Initialized

---
+++ src/main/java/com/excellent/excellentepubreader/ExcellentEpubReaderApplication.java
@@ -0,0 +1,13 @@
+package com.excellent.excellentepubreader;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExcellentEpubReaderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExcellentEpubReaderApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/excellent/excellentepubreader/ExcellentEpubReaderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.excellent.excellentepubreader;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExcellentEpubReaderApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
